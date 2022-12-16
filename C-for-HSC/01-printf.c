cb7008a3d941a91efbb5761cd2e2a7503814b81b
Hind Sagar Biswas <hindsbhk@gmail.com>
2022-12-16 20:08:29 +0600

    added c for hsc 1st lecture files

---
+++ C-for-HSC/01-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("My age is more than %d and less than %d", 18, 20 * 2);
+
+    return 0;
+}
--- C-for-HSC/1-print.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-void main()
-{
-    printf("I have 20 sugar cubes\n");
-    return;
-}
